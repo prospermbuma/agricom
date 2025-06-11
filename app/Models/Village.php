3fae929f0823e6ee26a6525e0722209b1a7585aa
prospermbuma <prospermbuma@gmail.com>
2025-06-11 07:36:54 +0300

    Updated village model logics

--- app/Models/Village.php
+++ app/Models/Village.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Village extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'region_id',
+        'code',
+        'latitude',
+        'longitude',
+        'population'
+    ];
+
+    protected $casts = [
+        'latitude' => 'decimal:8',
+        'longitude' => 'decimal:8',
+    ];
+
+    // Relationships
+    public function region()
+    {
+        return $this->belongsTo(Region::class);
+    }
+
+    public function farmerProfiles()
+    {
+        return $this->hasMany(FarmerProfile::class);
+    }
 }
