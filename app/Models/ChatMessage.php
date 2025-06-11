8ee7136f3fda33790130c3b67992cf080f44430a
prospermbuma <prospermbuma@gmail.com>
2025-06-11 09:57:16 +0300

    Updated ChatMessage logics

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChatMessage extends Model
 {
-    //
+    protected $fillable = [
+        'content',
+        'chat_room_id',
+        'user_id',
+        'read_at'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'read_at' => 'datetime',
+        ];
+    }
+
+    public function chatRoom()
+    {
+        return $this->belongsTo(ChatConversation::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
