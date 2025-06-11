2cb007edb0c301abe949a71f1b045aed6622acf3
prospermbuma <prospermbuma@gmail.com>
2025-06-11 09:52:35 +0300

    Updated logics of ChatConversation

--- app/Models/ChatConversation.php
+++ app/Models/ChatConversation.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChatConversation extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'type',
+        'crop_id',
+        'participants'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'participants' => 'array',
+        ];
+    }
+
+    public function crop()
+    {
+        return $this->belongsTo(Crop::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(ChatMessage::class);
+    }
+
+    public function getParticipantUsersAttribute()
+    {
+        return User::whereIn('id', $this->participants ?? [])->get();
+    }
 }
