84dfa1ce00656b169ecd61699f8ba6a9b48e3db8
prospermbuma <prospermbuma@gmail.com>
2025-06-11 08:10:27 +0300

    Created Comment Model

---
+++ app/Models/Comment.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
+
+class Comment extends Model
+{
+    use HasFactory, LogsActivity;
+
+    protected $fillable = [
+        'article_id', 'user_id', 'content', 'parent_id', 'is_approved'
+    ];
+
+    protected $casts = [
+        'is_approved' => 'boolean',
+    ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['content', 'is_approved'])
+            ->logOnlyDirty();
+    }
+
+    // Relationships
+    public function article()
+    {
+        return $this->belongsTo(Article::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Comment::class, 'parent_id');
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
+
+    public function scopeApproved($query)
+    {
+        return $query->where('is_approved', true);
+    }
+
+    public function scopeParent($query)
+    {
+        return $query->whereNull('parent_id');
+    }
+}
