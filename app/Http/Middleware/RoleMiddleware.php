9c4e1456d85de1fddf399889f459163cc300ef97
prospermbuma <prospermbuma@gmail.com>
2025-06-18 02:06:18 +0300

    Updated role-middleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -4,17 +4,18 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RoleMiddleware
 {
     public function handle(Request $request, Closure $next, ...$roles)
     {
-        if (!auth()->check()) {
+        if (!Auth::check()) {
             return response()->json(['message' => 'Unauthenticated'], 401);
         }
 
-        $user = auth()->user();
-        
+        $user = Auth::user();
+
         if (!in_array($user->role, $roles)) {
             return response()->json(['message' => 'Unauthorized'], 403);
         }
