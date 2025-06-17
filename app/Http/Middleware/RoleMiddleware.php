aca7a53ec00ea851c65ef47c42214328ed7a4dfd
prospermbuma <prospermbuma@gmail.com>
2025-06-17 23:24:07 +0300

    Added Role-middleware

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RoleMiddleware
+{
+    public function handle(Request $request, Closure $next, ...$roles)
+    {
+        if (!auth()->check()) {
+            return response()->json(['message' => 'Unauthenticated'], 401);
+        }
+
+        $user = auth()->user();
+        
+        if (!in_array($user->role, $roles)) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        return $next($request);
+    }
+}
