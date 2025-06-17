30a3624b2de33b3a073ece4dc9cb3c8e27efb0ad
prospermbuma <prospermbuma@gmail.com>
2025-06-17 03:48:10 +0300

    Added codes on villages

--- database/seeders/VillageSeeder.php
+++ database/seeders/VillageSeeder.php
@@ -10,10 +10,10 @@ class VillageSeeder extends Seeder
     public function run()
     {
         $villages = [
-            ['name' => 'Kikuyu', 'region_id' => 1],
-            ['name' => 'Majengo', 'region_id' => 2],
-            ['name' => 'Iyunga', 'region_id' => 3],
-            ['name' => 'Nyamagana', 'region_id' => 4]
+            ['name' => 'Kikuyu', 'region_id' => 1, 'code' => 'KIK'],
+            ['name' => 'Majengo', 'region_id' => 2, 'code' => 'MAJ'],
+            ['name' => 'Iyunga', 'region_id' => 3, 'code' => 'IYU'],
+            ['name' => 'Nyamagana', 'region_id' => 4, 'code' => 'NYA']
         ];
 
         foreach ($villages as $village) {
