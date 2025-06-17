7f7c6d39352b33a7d46a50750705eea1bcc1d3aa
prospermbuma <prospermbuma@gmail.com>
2025-06-17 22:27:58 +0300

    Added MessageSent

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\ChatMessage;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    public function __construct(ChatMessage $message)
+    {
+        $this->message = $message->load('user');
+    }
+
+    public function broadcastOn()
+    {
+        return new PresenceChannel('chat.' . $this->message->conversation_id);
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'message' => [
+                'id' => $this->message->id,
+                'content' => $this->message->message,
+                'user' => [
+                    'id' => $this->message->user->id,
+                    'name' => $this->message->user->name,
+                    'avatar' => $this->message->user->avatar,
+                ],
+                'created_at' => $this->message->created_at->toISOString(),
+            ]
+        ];
+    }
+}
